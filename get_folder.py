0b4e26438abb48c927275b95b0f45970a3ed3dac
davidsmenesesc <david86543@hotmail.com>
2023-11-08 10:12:51 -0500

    minor change

--- get_folder.py
+++ get_folder.py
@@ -3,10 +3,12 @@ import glob
 import sys
 def main(argv):
     print("This is the folder")
+    ReturnFolderName()
 def ReturnFolderName():
     directory='GenerativeLSTM\output_files'
     folders = glob.glob(os.path.join(directory, '*/'))
-    folder = max(folders, key=os.path.getctime)
+    most_recent_folder = max(folders, key=os.path.getctime)
+    folder = print(os.path.basename(os.path.normpath(most_recent_folder)))
     return folder
 if __name__ == "__main__":
     main(sys.argv[1:])
